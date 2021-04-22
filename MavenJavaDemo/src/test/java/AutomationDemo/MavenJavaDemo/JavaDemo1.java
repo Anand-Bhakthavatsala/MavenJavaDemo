2d979d91a0775bc632c16ed76cf1277588da5d4f
Anand-Bhakthavatsala <35788456+Anand-Bhakthavatsala@users.noreply.github.com>
2021-04-22 20:50:58 +0530

    one file

---
+++ MavenJavaDemo/src/test/java/AutomationDemo/MavenJavaDemo/JavaDemo1.java
@@ -0,0 +1,14 @@
+package AutomationDemo.MavenJavaDemo;
+
+public class JavaDemo1 {
+
+	public JavaDemo1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome");
+	}
+
+}
