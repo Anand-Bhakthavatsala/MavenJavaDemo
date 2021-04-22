0737efbcd523a5c9b77971bf5242625618ffa088
Anand-Bhakthavatsala <35788456+Anand-Bhakthavatsala@users.noreply.github.com>
2021-04-22 21:07:59 +0530

    test

---
+++ MavenJavaDemo/src/test/java/AutomationDemo/MavenJavaDemo/JavaDemo2.java
@@ -0,0 +1,16 @@
+package AutomationDemo.MavenJavaDemo;
+
+import org.testng.annotations.Test;
+
+public class JavaDemo2 {
+
+	
+
+	@Test 
+	public void welcome()
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Welcome");
+	}
+
+}
